dffd44b1c47c6cd88902b0a073836b56eae8d69d
Jordan White <jwhite34@uw.edu>
2024-01-31 19:16:48 -0800

    added imports and set random seed

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import os
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import tensorflow as tf
+from tensorflow.keras import layers
 
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tf.random.set_seed(123)

00e85ac85444dec305bec686dd1d2ba33a4be1af
Jordan White <jwhite34@uw.edu>
2024-02-03 13:24:02 -0800

    added logic to download dataset

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ import cv2
 import matplotlib.pyplot as plt
 
 tf.random.set_seed(123)
+
+# downloading the dataset
+annotation_folder = "/dataset/"
+if not os.path.exists(os.path.abspath(".") + annotation_folder):
+    annotation_zip = tf.keras.utils.get_file(
+        "val.tar.gz",
+        cache_subdir=os.path.abspath("."),
+        origin="http://diode-dataset.s3.amazonaws.com/val.tar.gz",
+        extract=True
+    )
